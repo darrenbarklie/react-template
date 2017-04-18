1422e15ab5f28acb65f4b03e1ed96ed7fab96e19
darrenbarklie <darrenbarklie@gmail.com>
2017-04-18 22:46:08 +0700

    Additional Components, SCSS & reset.css

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Layout from './js/Layout'
+import { BrowserRouter } from 'react-router-dom';
+
+import Layout from './js/Layout';
 
 require('./sass/base.scss');
 
 ReactDOM.render(
-  <div id="root">
-    <h1>This is the Root container</h1>
-    <Layout />
-	</div>,
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>,
   document.getElementById('root')
 );
--- js/Layout.js
+++ js/Layout.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import Header   from './components/Header';
+import Contact  from './components/Contact';
+import Signup   from './components/Signup';
+import Footer   from './components/Footer';
+
 export default class Layout extends React.Component {
   render() {
     return (
       <div className="layout">
-        <h1>This is the Layout component</h1>
+        <Header />
+        <Signup />
+        <Contact />
+        <Footer />
       </div>
     )
   }
