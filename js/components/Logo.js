1078ee374da302b24737bd7ac39fcce76076d96c
Darren Barklie <darrenbarklie@gmail.com>
2018-04-14 13:52:21 +0100

    Clean repo

--- js/components/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Logo extends React.Component {
-  render() {
-    return (
-      <img className="header-logo" src="../assets/svg/logo-test.svg" />
-    )
-  }
-}
