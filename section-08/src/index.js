0661216cccd2429ed43f64772b935fa48c948027
Darren Barklie <darrenbarklie@gmail.com>
2018-04-27 22:45:37 +0100

    Section 8 create React app

--- section-08/src/index.js
+++ section-08/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './index.scss';
-import './styles/_globals.scss';
-
-
-
-import App from './App/App';
-
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
