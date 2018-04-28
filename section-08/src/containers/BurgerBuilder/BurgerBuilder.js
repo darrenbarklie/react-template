669ac38acb4fd369c019d7d3b6ced90a5bcde61d
Darren Barklie <darrenbarklie@gmail.com>
2018-04-28 14:24:38 +0100

    Architecture and styling build controls

--- section-08/src/containers/BurgerBuilder/BurgerBuilder.js
+++ section-08/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
+
+
 class BurgerBuilder extends Component {
   
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
   
