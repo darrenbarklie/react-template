dc98f0ccc0e6af1d3dd8bb88afb5c1741556a029
darrenbarklie <darrenbarklie@gmail.com>
2018-06-20 07:16:29 +0100

    SideDrawer close on backdrop click

--- section-08/src/components/Layout/Layout.js
+++ section-08/src/components/Layout/Layout.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = ( props ) => (
-  <Auxiliary>
-    
-    <Toolbar />
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
 
-    <SideDrawer />
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
 
-    <main className={classes.Content}>
-      {props.children}
-    </main>
+  render() {
+    return (
+      <Auxiliary>
+          
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} />
 
-  </Auxiliary>
-);
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+
+      </Auxiliary>
+    )
+  }
+}
 
 export default Layout;
