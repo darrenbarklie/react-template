2482a1a083f3c2c6b217770bafd63f6267250802
darrenbarklie <darrenbarklie@gmail.com>
2018-06-25 02:55:32 +0100

    Dynamic routing for posts

--- section-09/src/containers/Blog/Posts/Posts.js
+++ section-09/src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
+import { Link } from 'react-router-dom';
 
 import './Posts.css';
 import Post from '../../../components/Post/Post';
@@ -12,6 +13,8 @@ class Posts extends Component {
 
   componentDidMount () {
     // Async get request, requires promise
+    //console.log(this.props);
+
     axios.get('/posts')
       .then(response => {
         const posts = response.data.slice(0,4);
@@ -39,11 +42,13 @@ class Posts extends Component {
 
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
-        return <Post
-            key={post.id}
+        return (
+        <Link to={'/' + post.id} key={post.id}>
+          <Post
             title={post.title}
             author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)} />
+            clicked={() => this.postSelectedHandler(post.id)} />      
+        </Link> )
       });
     }
 
