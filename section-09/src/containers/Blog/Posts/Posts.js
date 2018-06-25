3324e538492358be80c03883742614ca82639ff3
darrenbarklie <darrenbarklie@gmail.com>
2018-06-25 06:35:23 +0100

    Dynamic routing to posts

--- section-09/src/containers/Blog/Posts/Posts.js
+++ section-09/src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
 
 import './Posts.css';
 import Post from '../../../components/Post/Post';
@@ -34,7 +33,7 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id});
+    this.props.history.push({pathname: '/' + id});
   }
 
   render () {
@@ -43,12 +42,14 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
-        <Link to={'/' + post.id} key={post.id}>
-          <Post
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)} />      
-        </Link> )
+          //<Link to={'/' + post.id} key={post.id}>
+            <Post
+              key={post.id}
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)} />      
+          //</Link>
+        );
       });
     }
 
